595f1a0c85a3e267b31cf7578e41cc8ffb95d566
kritika <kritikaktripathi@gmail.com>
2020-08-12 20:40:50 +0530

    Delete function for the cards

--- Js-script/user-1.js
+++ Js-script/user-1.js
@@ -1,24 +1,24 @@
-var arr_user_1_blogs= []
+let arr_user_1_blogs= []
 
 window.addEventListener("DOMContentLoaded",function(){
-    var write_text_content = document.getElementById('write_text')
+    let write_text_content = document.getElementById('write_text')
     write_text_content.addEventListener('click' ,call_text_box)
 })
 
 function call_text_box(){
-  var text_box = document.getElementById('text_box')
+  let text_box = document.getElementById('text_box')
 
-  var text_area_container = document.createElement('div')
+  let text_area_container = document.createElement('div')
   text_area_container.setAttribute('class','form-group m-5 p-5')
 
-  var text_area_title= document.createElement("input")
+  let text_area_title= document.createElement("input")
   text_area_title.setAttribute("class", "form-control")
-  var text_area_html = document.createElement('textarea')
+  let text_area_html = document.createElement('textarea')
   text_area_html.setAttribute('class','form-control')
   text_area_html.setAttribute('rows','10')
   text_area_html.placeholder = "Give your thoughts!!"
 
-  var text_area_button = document.createElement('button')
+  let text_area_button = document.createElement('button')
   text_area_button.textContent = "Add"
   text_area_button.setAttribute('class','btn  btn-primary btn-lg')
   text_area_button.addEventListener('click',append_card_for_blog)
@@ -31,48 +31,52 @@ function call_text_box(){
 
 function append_card_for_blog(){
     
-    var append_card = document.getElementById('text_box')
+    let append_card = document.getElementById('text_box')
     
-    var create_blog_card = document.createElement('card')
+    let create_blog_card = document.createElement('card')
     create_blog_card.setAttribute('class','card border border-dark ')
 
-    var create_blog_container = document.createElement('div')
+    let create_blog_container = document.createElement('div')
     create_blog_container.setAttribute('class','card-header')
 
-    var create_blog_title = document.createElement('h3')
+    let create_blog_title = document.createElement('h3')
     create_blog_title.setAttribute('class','float-left')
 
-    var create_blog_user_pic = document.createElement('img')
+    let create_blog_user_pic = document.createElement('img')
     create_blog_user_pic.src = "./../photos/user-1_Rutvik_profile_pic.jpg"
     create_blog_user_pic.setAttribute('class','img-fluid rounded-circle blog_post_title_img float-left')
 
     create_blog_container.append( create_blog_user_pic,create_blog_title )
     create_blog_title.textContent = event.target.previousElementSibling.previousElementSibling.value
-    var title_to_pass= create_blog_title.textContent 
+    let title_to_pass= create_blog_title.textContent 
 
-    var create_blog_body = document.createElement('p')
+    let create_blog_body = document.createElement('p')
     create_blog_body.setAttribute('class','card-body')
     create_blog_body.textContent = event.target.previousElementSibling.value
-    var content_to_pass= create_blog_body.textContent
+    let content_to_pass= create_blog_body.textContent
 
-    var obj_user_1_blogs= {
+    let obj_user_1_blogs= {
 
       title: title_to_pass, content: content_to_pass
     }
   
 
-    var create_card_footer_blog = document.createElement('div')
+    let create_card_footer_blog = document.createElement('div')
     create_card_footer_blog.setAttribute('class','card-footer')
 
-    var create_footer_edit = document.createElement('div')  
+    let create_footer_edit = document.createElement('div')  
     create_footer_edit.setAttribute('class','float-left')
     create_footer_edit.innerHTML = `<i class="far fa-edit"></i>`
     create_footer_edit.addEventListener('click',edit_blog)
 
-    var create_footer_delete = document.createElement('div')
+    let create_footer_delete = document.createElement('div')
     create_footer_delete.setAttribute('class','float-right')
     create_footer_delete.innerHTML = `<i class="fas fa-trash-alt"></i>`
-    create_footer_delete.addEventListener('click',delete_blog)
+    create_footer_delete.addEventListener('click',()=>{
+        create_blog_card.remove()
+
+        delete_blog()
+    })
 
     create_card_footer_blog.append(create_footer_edit , create_footer_delete)
 
@@ -82,7 +86,6 @@ function append_card_for_blog(){
     arr_user_1_blogs.push(obj_user_1_blogs)
     console.log(arr_user_1_blogs)
     localStorage.setItem("user_1_blog_posts", JSON.stringify(arr_user_1_blogs))
-
     // console.log(create_blog_body)
 }
 
@@ -91,5 +94,17 @@ function edit_blog(){
 }
 
 function delete_blog(){
+    let title_to_be_deleted = event.target.parentElement.parentElement.previousElementSibling.previousElementSibling.lastChild.textContent
 
+    let user_blog_1 = JSON.parse(localStorage.getItem("user_1_blog_posts"))
+    
+    for( let i = 0 ; i < user_blog_1.length ; i++){
+        //let blog_posts = JSON.parse(user_blog_1[i])
+        if( user_blog_1[i].title === title_to_be_deleted){
+            user_blog_1.splice(i,1)
+        }
+    }
+    console.log(user_blog_1)
+    user_blog_1 = JSON.stringify(user_blog_1)
+    localStorage.setItem('user_1_blog_posts',user_blog_1)
 }
